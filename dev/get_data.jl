a6a1e69583f9382d056fde571dfc1ff8f6581480
pat-alt <altmeyerpat@gmail.com>
2024-01-14 10:19:44 +0100

    getting data from repo

---
+++ dev/get_data.jl
@@ -0,0 +1,13 @@
+using Artifacts, ArtifactUtils
+import CodecZlib, Downloads, Tar
+
+# Raw data folder:
+raw_data_dir = "dev/data/fomc-hawkish-dovish-main/data"
+
+if !isdir(raw_data_dir)
+    tgz = Downloads.download("https://github.com/gtfintechlab/fomc-hawkish-dovish/archive/refs/heads/main.tar.gz")
+    open(GzipDecompressorStream, tgz) do io
+        Tar.extract(x -> contains(x.path, "fomc-hawkish-dovish-main/data"), io, "dev/data")
+    end
+end
+
---
+++ dev/pre_process.jl
@@ -0,0 +1,3 @@
+# Get data from the web and save it to a file
+include("get_data.jl")
+
