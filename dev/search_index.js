3638412b61148ad0ed726ee8f95cdaaf2e155f95
Documenter.jl <documenter@juliadocs.github.io>
2024-01-14 09:21:36 +0000

    build based on a6a1e69

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TrillionDollarWords","category":"page"},{"location":"#TrillionDollarWords","page":"Home","title":"TrillionDollarWords","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TrillionDollarWords.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TrillionDollarWords]","category":"page"}]
+}
