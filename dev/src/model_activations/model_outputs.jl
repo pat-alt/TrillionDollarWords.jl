c446aac87d5c446c49130abefbf78aca77773118
pat-alt <altmeyerpat@gmail.com>
2024-01-16 12:18:54 +0100

    slurm and removed nightly

--- dev/src/model_activations/model_outputs.jl
+++ dev/src/model_activations/model_outputs.jl
@@ -4,13 +4,14 @@ using Transformers
 using TrillionDollarWords
 
 # GPU:
+Transformers.enable_gpu()
 
 # Load model and data:
 mod = load_model(; load_head=false, output_hidden_states=true)
 df = load_all_sentences()
 
 # Compute activations:
-n = 100
+n = 10
 queries = df[1:n,:]
 emb = layerwise_activations(mod, queries)
 CSV.write("dev/data/activations/activations.csv", emb)
