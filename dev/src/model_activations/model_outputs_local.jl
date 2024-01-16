e1edd88ed04d114cec1d40a8f36dc585aaeceaac
pat-alt <altmeyerpat@gmail.com>
2024-01-16 14:24:02 +0100

    readme

---
+++ dev/src/model_activations/model_outputs_local.jl
@@ -0,0 +1,33 @@
+using Base.Iterators: partition
+using CSV
+using DataFrames
+using TrillionDollarWords
+
+# Load model and data:
+mod = load_model(; load_head=false, output_hidden_states=true)
+df = load_all_sentences()
+
+# Compute activations:
+bs = 10
+n_thr = Threads.nthreads()
+n_per_chunk = bs * n_thr
+n = size(df, 1)
+out_dir = tempdir()
+for (i, chunk) in enumerate(partition(1:n, n_per_chunk))
+    @info "Processing chunk $i/$(ceil(Int, n/n_per_chunk))..."
+    Threads.@threads for batch in collect(partition(chunk, bs))
+        println("$batch on $(Threads.threadid())")
+        queries = df[batch,:]
+        emb = layerwise_activations(mod, queries)
+        CSV.write(joinpath(out_dir, "activations_$batch.csv"), emb)
+    end
+end
+
+# Merge:
+df_activations = []
+for x in readdir(out_dir)[contains.(readdir(out_dir), ".csv")]
+    push!(df_activations, CSV.read(joinpath(out_dir, x), DataFrame))
+end
+df_activations = vcat(df_activations...) |>
+    x -> sort!(x, [:sentence_id, :layer, :activation_id]) 
+CSV.write("dev/data/activations/activations.csv", df_activations)
