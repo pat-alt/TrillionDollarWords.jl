6bfbd0d41e2ca64279839734e4853d2c71018bad
pat-alt <altmeyerpat@gmail.com>
2024-01-19 10:05:16 +0100

    updated doc string

--- src/baseline_model.jl
+++ src/baseline_model.jl
@@ -79,7 +79,7 @@ end
 """
     laywerwise_activations(mod::BaselineModel, queries::Vector{String})
 
-Computes a forward pass of the model on the given queries and returns the layerwise activations for the `HGFRobertaModel`. If `output_hidden_states=false` was passed to `load_model` (default), only the last layer is returned. If `output_hidden_states=true` was passed to `load_model`, all layers are returned. Even if the model is loaded with the head for classification, the head is not used for computing the activations.
+Computes a forward pass of the model on the given queries and returns the layerwise activations for the `HGFRobertaModel`. If `output_hidden_states=false` was passed to `load_model` (default), only the last layer is returned. If `output_hidden_states=true` was passed to `load_model`, all layers are returned. If the model is loaded with the head for classification, the activations going into the final linear layer are returned.
 """
 function layerwise_activations(mod::BaselineModel, queries::Vector{String})
     embeddings = get_embeddings(mod, queries)
