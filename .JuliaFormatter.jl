6aeb2ff46bc170df143e5481b525870cebd460b4
pat-alt <altmeyerpat@gmail.com>
2024-01-19 08:19:46 +0100

    formatter

--- .JuliaFormatter.jl
+++ .JuliaFormatter.jl
@@ -2,6 +2,6 @@ using Pkg                 # Load package manager
 Pkg.add("JuliaFormatter") # Install JuliaFormatter
 
 using JuliaFormatter      # Load JuliaFormatter
-format("."; verbose=true) # Format all files 
+format("."; verbose = true) # Format all files 
 
 Pkg.rm("JuliaFormatter")  # Remove JuliaFormatter
